ec2b3cf3a5a4c588665e59a37869c9b92ebba2d7
Kirill Zemko <therealkirill@gmail.com>
2025-07-22 21:37:22 +0200

    add render cards from initial-cards

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// import { initialCards } from './initial-cards.js'; // список карточек
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let editBtn = document.querySelector('.btn_type_edit');
@@ -9,25 +37,52 @@ let nameInput = formElement.querySelector('.form__input_el_name');
 let jobInput = formElement.querySelector('.form__input_el_description');
 let likeButtons = document.querySelectorAll('.btn_type_like');
 
-function handleFormSubmit() {
-  // evt.preventDefault();
+let cardsContainer = document.querySelector('.cards'); // переменная для контейнера карточек
+// переменные шаблона card-template
+const cardTemplate = document.querySelector('#card-template').content; // Добавляем .content для доступа к содержимому шаблона
 
-  profileName.textContent = nameInput.value;
+// функция создания карточки, принимает объект карточки cardData и возвращает карточку с картинкой и текстом
+function createCard(cardData) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); // клонируем элемент
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = cardData.link; // устанавливаем ссылку на картинку
+  cardImage.alt = cardData.name; // устанавливаем альт-текст
+  cardTitle.textContent = cardData.name; // устанавливаем заголовок
+
+  return cardElement; // возвращаем карточку
+}
+
+// отрисовка начальных карточек на странице
+initialCards.forEach(function(cardData) {
+  const card = createCard(cardData);
+  cardsContainer.append(card);
+});
+
+function handleFormSubmit(evt) {
+  console.log('Функция handleFormSubmit вызвана!');
+
+  evt.preventDefault();
+
+  console.log('click');
+
+  profileName.textContent = nameInput.value.toUpperCase().slice(0, 1) + nameInput.value.slice(1);
   profileDescription.textContent = jobInput.value;
 
   popup.classList.toggle('popup_opened');
 }
 
-editBtn.addEventListener('click', function() {
+function editPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileDescription.textContent;
 
   popup.classList.toggle('popup_opened');
-});
+}
 
-popupCloseBtn.addEventListener('click', function closePopup() {
+function closePopup() {
   popup.classList.toggle('popup_opened');
-});
+}
 
 likeButtons.forEach(function(button) {
   button.addEventListener('click', function() {
@@ -36,4 +91,6 @@ likeButtons.forEach(function(button) {
   });
 });
 
+editBtn.addEventListener('click', editPopup);
+popupCloseBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
