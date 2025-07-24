b5070699acae040b59fa00f20d1f4d27ca75c54f
Kirill Zemko <therealkirill@gmail.com>
2025-07-24 14:25:55 +0200

    refactor: external initial-cards file

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,21 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-// import { initialCards } from './initial-cards.js'; // список карточек
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let editBtn = document.querySelector('.btn_type_edit');
-let addCardBtn = document.querySelector('.btn_type_add');
-let btnSubmit = document.querySelector('.btn_type_save');
-let popup = document.querySelector('.popup');
-let popupCloseBtn = document.querySelector('.popup__close-icon');
-let formElement = document.querySelector('.form__input-container');
-let nameInput = formElement.querySelector('.form__input_el_name');
-let jobInput = formElement.querySelector('.form__input_el_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const editBtn = document.querySelector('.btn_type_edit');
+const addCardBtn = document.querySelector('.btn_type_add');
+const btnSubmit = document.querySelector('.btn_type_save');
+const popup = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.popup__close-icon');
+const formElement = document.querySelector('.form__input-container');
+const nameInput = formElement.querySelector('.form__input_el_name');
+const jobInput = formElement.querySelector('.form__input_el_description');
 // переменные addCardPopup
-let addCardPopup = document.querySelector('.popup_type_add-card');
-let formAddCardPopup = document.querySelector('.form_type_add-card');
-let cardNameInput = document.querySelector('.form__input_el_card-name');
-let cardLinkInput = document.querySelector('.form__input_el_card-link');
-let addCardPopupCreateBtn = document.querySelector('.btn_type_create');
-let addCardPopupCloseBtn = document.querySelector('.popup-add-card-close-btn');
-let cardsContainer = document.querySelector('.cards'); // переменная для контейнера карточек
+const addCardPopup = document.querySelector('.popup_type_add-card');
+const formAddCardPopup = document.querySelector('.form_type_add-card');
+const cardNameInput = document.querySelector('.form__input_el_card-name');
+const cardLinkInput = document.querySelector('.form__input_el_card-link');
+const addCardPopupCreateBtn = document.querySelector('.btn_type_create');
+const addCardPopupCloseBtn = document.querySelector('.popup-add-card-close-btn');
+const cardsContainer = document.querySelector('.cards'); // переменная для контейнера карточек
 const cardTemplate = document.querySelector('#card-template').content; // Добавляем .content для доступа к содержимому шаблона
 // переменные popup_type_preview
 const previewPopup = document.querySelector('.popup_type_preview');
